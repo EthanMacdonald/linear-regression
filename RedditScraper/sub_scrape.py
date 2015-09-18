7196bda53b4d2f76110b2c54d10aecd18a5971e6
Ethan Macdonald <ethan.bryce.macdonald@gmail.com>
2015-09-18 04:54:17 -0400

    Add subreddit scraper

---
+++ RedditScraper/sub_scrape.py
@@ -0,0 +1,24 @@
+import json, praw, os, time
+
+data_dir = "../data/reddit/json/sub"
+url = 'https://www.reddit.com/r/all/new'
+
+params = {'limit': 100}
+i = 0
+while True:
+	try: 
+		if i % 20 == 0:
+			time.sleep(60)
+		r = praw.Reddit(user_agent='laptop:YnAFbPkdUgE0Ig:1.0 (by /u/ShittyAlgorithms)')
+		r.config.store_json_result = True
+		submissions = r.request_json(url, params=params, as_objects=False)
+		submissions['scrape_time_utc'] = time.time()
+		after = submissions['data']['after']
+		params = {'limit': 100, 'after': after}
+		with open(os.path.join(data_dir, after+'.json'), 'w+') as f:
+			json.dump(submissions, f)
+		i += 1
+		print "Submissions saved: " + str(i*100)
+	except Exception as e:
+		print e
+		continue
