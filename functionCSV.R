4a7fea5d5aff4fbef4d7c1315571da7e45978b8d
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2015-09-18 23:38:58 -0400

    Update functionCSV.R

--- functionCSV.R
+++ functionCSV.R
@@ -1,10 +1,5 @@
 
 
-#jsonData3<- fromJSON("t3_3l901y.json", flatten = TRUE)
-#jsonData4 <- jsonData3$data$children
-#jsonData4 <- jsonData4[,lapply(jsonData4,class)!= "list"]
-#lengthTrainingM <- dim(jsonData4)[1]
-#jsonData4 <- jsonData4[,lapply(jsonData4,length) == lengthTrainingM]
 
 functionCSV <-  function(fileNameJSON){
   library(jsonlite)
