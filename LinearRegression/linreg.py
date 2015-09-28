6891236e4d0461c07a5276c5a015009f99adf2bb
Ethan Macdonald <ethan.bryce.macdonald@gmail.com>
2015-09-28 19:59:35 -0400

    Improve linreg, run. Add regularization

--- LinearRegression/linreg.py
+++ LinearRegression/linreg.py
@@ -4,39 +4,36 @@ import numpy as np
 #REF: COMP 598 Lecture slides
 #REF: http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html to make sure I didn't miss any important methods - I didn't look at any code!
 
-#TODO: Regularization (optional)
-#TODO: Extra features (optional)
-#TODO: Optimize matrix operations (optional)
-
 class LinearRegression(object):
 
 	def __init__(self, copy=True):
 		self.copy = copy
 
-	def closed_fit(self, X, y, regularize=False):
+	def closed_fit(self, X, y, reg_factor):
 		# Closed form fit using matrix inversion
 		if self.copy:
 			X = np.copy(X)
 			y = np.copy(y)
-		self.w = np.linalg.inv(np.transpose(X).dot(X)).dot(np.transpose(X)).dot(y)
+		r, c = X.shape
+		self.w = np.linalg.inv(np.transpose(X).dot(X)+reg_factor*np.identity(c)).dot(np.transpose(X)).dot(y)
 		return np.copy(self.w)
 
-	def gradient_fit(self, X, y, alpha, err, regularize=False):
+	def gradient_fit(self, X, y, alpha, err, reg_factor):
 		# Gradient descent fit
 		if self.copy:
 			X = X.copy()
 			y = y.copy()
 		r, c = X.shape
-		w0 = np.transpose(np.array([[1]*c]))
-		self.w = self._next_weight(w0, X, y, alpha)
+		w0 = np.transpose(np.array([[1]*c]))    
+		self.w = self._next_weight(w0, X, y, alpha,reg_factor)
 		while np.sum(abs(w0 - self.w))/len(w0) > err:
 			w0 = self.w
-			self.w = self._next_weight(w0, X, y, alpha)
+			self.w = self._next_weight(w0, X, y, alpha,reg_factor)
 		return self.w
 
-	def _next_weight(self, w, X, y, alpha):
+	def _next_weight(self, w, X, y, alpha,reg_factor):
 		# Helper function for gradient_fit method
-		return w - alpha*(np.transpose(X).dot(X).dot(w)-np.transpose(X).dot(y))
+		return w - alpha*(np.transpose(X).dot(X).dot(w)-np.transpose(X).dot(y)) - (alpha*reg_factor*w)
 
 	def predict(self, X):
 		# Predicts an output for a given X
@@ -56,12 +53,14 @@ class LinearRegression(object):
 		err = np.transpose(inner).dot(inner)/r
 		return err
 
-	def compare_error(self, X_train, y_train, X_test, y_test, alpha, err):
+	def compare_error(self, X_train, y_train, X_test, y_test, alpha, err,reg_factor):
 		# Compares gradient descent error and closed form error
-		self.gradient_fit(X_train, y_train, alpha, err)
-		gradient_test = self.test(X_test, y_test)
-		self.closed_fit(X_train, y_train)
+		self.gradient_fit(X_train, y_train, alpha, err,reg_factor)
+		gradient_test = self.test(X_train, y_train)
+		grad_predict = self.predict(X_test)
+		print "Gradnt Err: " + str(self.test(X_train,y_train))
+		self.closed_fit(X_train, y_train,reg_factor)
 		closed_test = self.test(X_test, y_test)
-		# Print results of comparison
-		print "Gradnt Err: " + str(gradient_test)
-		print "Closed Err: " + str(closed_test)
+		print "Closed Err: " + str(self.test(X_train,y_train))
+		closed_predict = self.predict(X_test)
+		return closed_predict, grad_predict # Returns closed prediction and grad prediction
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import csv, os
 import numpy as np
+import random
 from numpy import genfromtxt
 from LinearRegression.linreg import LinearRegression
+import math
+import matplotlib.pyplot as plt
 
 #NOTE: Closed fit works better, but gradient descent avoids inverting matricies
 
@@ -13,7 +16,7 @@ y_data = [2.49, .83, -.25, 3.1, .87, .02, -.12, 1.81, -.83, .43]
 x0 = [1]*len(x_data)
 X = np.transpose(np.array([x0, x_data]))
 y = np.transpose(np.array([y_data]))
-# lr.compare_error(X, y, .05, .0000001)
+#lr.compare_error(X, y, .05, .0000001)
 
 # Synthetic data from a linear function
 y = lambda x: 10*x -10
@@ -21,21 +24,85 @@ x_data = [i for i in range(10)]
 x0 = [1]*len(x_data)
 X = np.transpose(np.array([x0, x_data]))
 y = np.transpose(np.array([map(y, x_data)]))
-# lr.compare_error(X, y, .0045, .0000000000000000001)
+#lr.compare_error(X, y, .0045, .0000000000000000001)
+
 
 # Assignment data
 data_dir = "data/OnlineNewsPopularity"
 csv_path = os.path.join(data_dir, "OnlineNewsPopularity.csv")
 csv = genfromtxt(csv_path, delimiter=', ', skip_header=1)
 r, c = csv.shape
-h_split_1 = int(.33*r)
-h_split_2 = int(.66*r)
-arrays = [np.array_split(csv, [1, 60], axis=1) for i in np.array_split(csv, [h_split_1, h_split_2], axis=0)] #TODO: Ask Pineau about removing the first column?
-urls = [i[0] for i in arrays]
-X_train = arrays[0][1]
-X_valid = arrays[1][1]
-X_test = arrays[2][1]
-y_train = arrays[0][2]
-y_valid = arrays[1][2]
-y_test = arrays[2][2]
-lr.compare_error(X_train, y_train, X_valid, y_valid, .00000000000000004, .01)
+
+X_train = []
+X_test = []
+y_train = []
+y_test = []
+test_portion =0.30
+for i in csv:
+        rd = random.random()
+        if rd < test_portion: 
+            spl = np.array_split(i, [1, 60])
+            X_test.append(spl[1])
+            y_test.append(spl[2])
+            
+        else :
+            spl = np.array_split(i, [1, 60])
+            X_train.append(spl[1])
+            y_train.append(spl[2])
+
+X_train=np.array(X_train)
+y_train=np.array(y_train)
+X_test=np.array(X_test)
+y_test=np.array(y_test)
+
+error =[[],[]]
+min_result = 2e+50
+
+#h_split_1 = int(.33*r)
+#csv = csv[:, [1,29,60]] # Only keeps some rows
+#arrays = [np.array_split(csv, [1, 60], axis=1) for i in np.array_split(csv, [h_split_1,2*h_split_1], axis=0)] #TODO: Ask Pineau about removing the first column?
+#urls = [i[0] for i in arrays]
+#X_train = arrays[0][1]
+#X_test = arrays[1][1]
+#y_train = arrays[0][2]
+#y_test = arrays[1][2]
+#urls = [i[0] for i in arrays]
+#for i in range(1,25) :
+#   for j in range(1):
+#        result = lr.compare_error(X_train, y_train, X_valid, y_valid, math.pow(10,-17), .01,math.pow(2,-i))[0][0]
+#        error[0].append(result)
+#        error[1].append(1/(math.pow(2,i)))
+#        if result < min_result   :
+#            min_i =i
+#            min_result=result
+#        print result
+#print min_result, min_i
+i=17
+ret_closed, ret_grad= lr.compare_error(X_train, y_train, X_test, y_test, math.pow(10,-i), .000001,0)
+print ret_closed,ret_grad
+i=1
+grad=[]
+closed=[]
+train=[]
+min_size = min(len(ret_grad),len(ret_closed),len(y_train))
+#Makes sure all arrays have the same length
+while (i<min_size):
+    if i%1==0:
+        grad.append(ret_grad[i])
+        closed.append(ret_closed[i])
+        train.append(y_train[i])
+    i=i+1
+#plt.plot(range(len(ret_y)),ret_y,'ro', range(len(ret_y)),ret_x,'bs')
+p1 = plt.plot(range(len(grad)),grad,'bs',markersize=5) # Plots the predictions for Gradient descent
+#p2 = plt.plot(range(len(a)),b,'ro',markersize=3)
+p3 = plt.plot(range(len(grad)),train,'yo',markersize=5) # Plots the real values of the training set
+#plt.plot(range(len(ret_x)),ret_x,'bs',range(len(ret_y)),ret_y,'ro')
+
+#plt.plot(error[1], error[0], 'ro')
+#plt.xscale('log',basex=10)
+#plt.axis([1,0,0,1.53170261e+8])
+plt.title('Predicting the number of shares')
+plt.ylabel('Number of shares')
+plt.xlabel('Index (0 = most recent article)')
+#plt.legend([p1, p2], ["Gradient descent prediction", "Actual number of shares"])
+plt.show()
