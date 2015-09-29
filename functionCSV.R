83c0596bfdebf5821aa5173d52dbd4cbb66aae14
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2015-09-29 11:08:22 -0400

    comments
    comments

--- functionCSV.R
+++ functionCSV.R
@@ -1,4 +1,4 @@
-
+#This function takes a JSON filenames as input and outputs the corresponding csv file.
 functionCSV <-  function(fileNameJSON){
   library(jsonlite)
   jsonData3<- fromJSON(fileNameJSON, flatten = TRUE)
@@ -6,13 +6,8 @@ functionCSV <-  function(fileNameJSON){
   jsonData5 <- jsonData5[,lapply(jsonData5,class)!= "list"]
   lengthTrainingM <- dim(jsonData5)[1]
   jsonData5 <- jsonData5[,lapply(jsonData5,length) == lengthTrainingM]
- 
- 
   csvName <- sub(".json",".csv",fileNameJSON)
   write.csv(jsonData5,csvName)
-  
-
-  
   
   
 }
