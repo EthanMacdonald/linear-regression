8fbfa64bfd77a40ef6e00e5c400c367d9fcd7398
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2015-09-29 00:01:13 -0400

    change
    cahnge

--- functionCSV.R
+++ functionCSV.R
@@ -1,16 +1,17 @@
 
-
-
 functionCSV <-  function(fileNameJSON){
   library(jsonlite)
   jsonData3<- fromJSON(fileNameJSON, flatten = TRUE)
-  jsonData4 <- jsonData3$data$children
-  jsonData4 <- jsonData4[,lapply(jsonData4,class)!= "list"]
-  lengthTrainingM <- dim(jsonData4)[1]
-  jsonData4 <- jsonData4[,lapply(jsonData4,length) == lengthTrainingM]
+  jsonData5 <- jsonData3$data$children
+  jsonData5 <- jsonData5[,lapply(jsonData5,class)!= "list"]
+  lengthTrainingM <- dim(jsonData5)[1]
+  jsonData5 <- jsonData5[,lapply(jsonData5,length) == lengthTrainingM]
+ 
+ 
   csvName <- sub(".json",".csv",fileNameJSON)
-  write.csv(jsonData4,csvName)
+  write.csv(jsonData5,csvName)
   
+
   
   
   
