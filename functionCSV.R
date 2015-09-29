634abd0e7085382e26eec94b41515a353ff278d0
AndresFelipeRG <andresfeliperincongamboa@web127011.wireless.mcgill.ca>
2015-09-28 22:10:08 -0400

    Revert "Added some R code I used to create the cvs file, select the features and manipulate some variables"
    This reverts commit 978cd5591697d2fd2043cb41dc9122c626fac70c.

--- functionCSV.R
+++ functionCSV.R
@@ -1,18 +1,22 @@
 
 
+#jsonData3<- fromJSON("t3_3l901y.json", flatten = TRUE)
+#jsonData4 <- jsonData3$data$children
+#jsonData4 <- jsonData4[,lapply(jsonData4,class)!= "list"]
+#lengthTrainingM <- dim(jsonData4)[1]
+#jsonData4 <- jsonData4[,lapply(jsonData4,length) == lengthTrainingM]
+
 functionCSV <-  function(fileNameJSON){
   library(jsonlite)
   jsonData3<- fromJSON(fileNameJSON, flatten = TRUE)
-  jsonData5 <- jsonData3$data$children
-  jsonData5 <- jsonData5[,lapply(jsonData5,class)!= "list"]
-  lengthTrainingM <- dim(jsonData5)[1]
-  jsonData5 <- jsonData5[,lapply(jsonData5,length) == lengthTrainingM]
- 
- 
+  jsonData4 <- jsonData3$data$children
+  jsonData4 <- jsonData4[,lapply(jsonData4,class)!= "list"]
+  lengthTrainingM <- dim(jsonData4)[1]
+  jsonData4 <- jsonData4[,lapply(jsonData4,length) == lengthTrainingM]
   csvName <- sub(".json",".csv",fileNameJSON)
-  write.csv(jsonData5,csvName)
-  
-
+  write.csv(jsonData4,csvName)
+  print(dim(jsonData4[1]))
+  print(dim(jsonData4[2]))
   
   
   
