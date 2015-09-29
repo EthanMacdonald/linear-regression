3cdb3592a0dedb4c17b75435005302f0fec577ed
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2015-09-29 11:13:33 -0400

    comments
    comments

--- makeOneCSVFile.R
+++ makeOneCSVFile.R
@@ -1,3 +1,6 @@
+#This function takes as input a list "csvlist" which represents a list of data frames, corresponding to csv data, and indexes "indexA" 
+#and "indexB" are indexes of the list "csvlist". The function outputs a merged data frame "csv" containing all the data from "indexA" to "indexB"
+#in the csvlist.
 makeOneCSVFile <- function(csvlist, indexA, indexB){
   csv <- csvlist[[indexA]]
   for (i in ((indexA + 1): indexB)){
